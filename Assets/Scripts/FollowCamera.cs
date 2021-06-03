1dca03729fe4247a5f276488803e35e76f9c520f
astaphya <ender7tml@gmail.com>
2021-06-03 23:37:19 +0300

    Kamera takibi daha pürüzsüz hale getirildi.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,40 +5,20 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform Cube;
-   // [SerializeField] private Vector3 offSet;
-    private float lerpTime = 0.1f;
-
-    public Vector3 dist;
+    [SerializeField] private Vector3 offSet;
+    [SerializeField]private float speed;
     
    
-    void LateUpdate()
+    private void LateUpdate()
     {
-        transform.position = Cube.position + dist;
-        //transform.LookAt(Cube.position);
-       // transform.position = Vector3.SmoothDamp(transform.position,Cube.position+offSet,ref velocity,lerpTime);
+         transform.position =  Vector3.Lerp(transform.position,Cube.position+offSet , speed * Time.deltaTime);
     }
     
-    /*
+ 
     
-    private void FixedUpdate()
-    {
-        while(GameManager.Instance.isAktif)
-        {
-            ChangeTheCam(camTarget);
-        }
-        
-        
-    }
+ 
 
-    public void ChangeTheCam(Transform cameraTarget)
-    {
-        Vector3 dPos = cameraTarget.position + dist;
-        Vector3 sPos = Vector3.Lerp(transform.position,dPos,speed*Time.deltaTime);
-        transform.position = sPos;
-        transform.LookAt(lookTarget.position);
-
-    }
-    */
+  
 
 
 }
